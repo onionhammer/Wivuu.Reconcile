3dcedc8ec9389c02312bd3941f3bb9191ac4bc55
Erik O'Leary <erik.m.oleary@gmail.com>
2018-04-20 13:26:21 -0500

    Added more test coverage

--- Wivuu.Reconcile.Tests/TestReconciliation.cs
+++ Wivuu.Reconcile.Tests/TestReconciliation.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Wivuu.Reconcile.Tests
@@ -53,6 +55,79 @@ namespace Wivuu.Reconcile.Tests
 
             foreach (var i in expected)
                 Assert.IsTrue(destItems.Contains(i));
+        }
+
+        class TestItem
+        {
+            public string Id { get; }
+
+            public string Email { get; set; }
+
+            public TestItem(string id, string email)
+            {
+                this.Id    = id;
+                this.Email = email;
+            }
+        }
+
+        [TestMethod]
+        public void TestZipUpdate()
+        {
+            var userInput = new[] { new TestItem("1", "test.user@email.com") };
+            var database  = new[] {
+                new TestItem("1", "test.user@emailz.com"),
+                new TestItem("2", "second.user@email.com"),
+            };
+
+            var expected = new[]
+            {
+                new TestItem("1", "test.user@email.com"),
+                new TestItem("2", "second.user@email.com"),
+            };
+
+            database
+                .Reconcile(userInput, (src, dest) => src.Id == dest.Id)
+                .WithMatching((src, dest) =>
+                {
+                    dest.Email = src.Email;
+                })
+                .Done();
+
+            foreach (var i in expected)
+                Assert.IsTrue(database.Any(d => d.Email == i.Email));
+        }
+
+        [TestMethod]
+        public void TestZipAddDeleteUpdate()
+        {
+            var userInput = new[] {
+                new TestItem("1", "test.user@email.com"),
+                new TestItem("3", "third.user@email.com"),
+            };
+
+            var database = new List<TestItem> {
+                new TestItem("1", "test.user@emailz.com"),
+                new TestItem("2", "second.user@email.com"),
+            };
+
+            var expected = new[]
+            {
+                new TestItem("1", "test.user@email.com"),
+                new TestItem("3", "third.user@email.com")
+            };
+
+            database
+                .Reconcile(userInput, (src, dest) => src.Id == dest.Id)
+                .WithMatching((src, dest) =>
+                {
+                    dest.Email = src.Email;
+                })
+                .WithItemNotInSource(dest => database.Remove(dest))
+                .WithItemNotInDestination(source => database.Add(source))
+                .Done();
+
+            foreach (var i in expected)
+                Assert.IsTrue(database.Any(d => d.Email == i.Email));
         }
     }
 }
